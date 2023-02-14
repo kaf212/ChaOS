d22f4ce23c9a1a6b96233b0ccc1d1066d3a87e33
kaf212 <kaf221122@gmail.com>
2023-02-14 16:18:50 +0100

    various improvements around csv writing in login() and co.

--- login.py
+++ login.py
@@ -10,7 +10,30 @@ def login():
 
     username = None
     while username is None:
-        input_username = input('Username > ')
+        input_username = input('Username (type /register to create new account) > ')
+        if input_username == '/register':
+            name_taken = True
+            while name_taken:
+                input_username = input('Enter your username > ')
+
+                with open('users.csv', 'r') as csv_file:
+                    attributes = ['name', 'password']
+                    next(csv_file)  # skip attribute header
+                    csv_reader = csv.DictReader(csv_file, fieldnames=attributes)
+
+                    name_taken = False
+                    for line in csv_reader:
+                        if line['name'] == input_username:
+                            name_taken = True
+
+                    if name_taken:
+                        input('This Username is already taken. ')
+                    else:
+                        pass #todo: implement create user()
+
+
+
+
 
         with open('users.csv', 'r') as csv_file:
             attributes = ['name', 'password']
@@ -40,3 +63,21 @@ def login():
         exit()
 
 
+
+def create_user(username, password):
+    with open('users.csv', 'a+') as csv_file:
+        attributes = ['username', 'password']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=attributes)
+        csv_writer.writerow({'username': username, 'password': password})
+
+        csv_file.close()
+
+def initialize_users():
+    with open('users.csv', 'w') as csv_file:
+        attributes = ['username', 'password']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=attributes)
+        csv_writer.writerow({'username': 'kaf212', 'password': '1234'})
+        csv_writer.writerow({'username': 'NextToNothing', 'password': 'asdf'})
+        csv_writer.writerow({'username': 'Custoomer31', 'password': 'svp'})
+        csv_writer.writerow({'username': 'Seve', 'password': 'minecraft'})
+        csv_file.close()
