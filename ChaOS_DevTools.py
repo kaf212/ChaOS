f23b6c841a745345d12bc36718c5a7f37255446b
kaf212 <115266756+kaf212@users.noreply.github.com>
2023-02-23 13:48:39 +0100

    Documented ChaOS_DevTools.py

--- ChaOS_DevTools.py
+++ ChaOS_DevTools.py
@@ -5,12 +5,14 @@ from encryption import *
 
 def reset_user_csv(cmd_split):
     """
-    Genocide tool for test users.  dev reset user_csv -hard
+    Genocide tool for test users.
+    normal reset: only deletes users with "test" in their name.
+    hard reset: deletes all non default users.
     :return:
     """
     reset_flag = None
     try:
-        reset_flag = cmd_split[3]
+        reset_flag = cmd_split[3]   # if a flag is present
     except IndexError:
         pass
 
@@ -18,7 +20,7 @@ def reset_user_csv(cmd_split):
         with open('users.csv', 'w', encoding="utf-8") as csv_file:
             attributes = ChaOS_constants.USER_CSV_ATTRIBUTES
             csv_writer = csv.DictWriter(csv_file, fieldnames=attributes)
-            csv_writer.writeheader()
+            csv_writer.writeheader()  #TODO: insert encrypted password to kaf221122
             csv_writer.writerow(
                 {'name': 'kaf221122', 'password': encrypt_str(decrypt_str('')), 'account type': 'dev'})
             csv_writer.writerow(
@@ -32,7 +34,7 @@ def reset_user_csv(cmd_split):
             csv_file.close()
     else:
         temp_dict_list = []
-        with open('users.csv', 'r', encoding='utf-8') as csv_file:
+        with open('users.csv', 'r', encoding='utf-8') as csv_file:   # read in every user in users.csv and add it to a temp dict if it doesnt contain "test"
             attributes = ChaOS_constants.USER_CSV_ATTRIBUTES
             csv_reader = csv.DictReader(csv_file, fieldnames=attributes)
             next(csv_file)
@@ -40,7 +42,7 @@ def reset_user_csv(cmd_split):
                 if 'test' not in line['name']:
                     temp_dict_list.append(line)
 
-        with open('users.csv', 'w', encoding='utf-8') as csv_file:
+        with open('users.csv', 'w', encoding='utf-8') as csv_file:  # overwrite the csv with the temp dict
             attributes = ChaOS_constants.USER_CSV_ATTRIBUTES
             csv_writer = csv.DictWriter(csv_file, fieldnames=attributes)
             csv_writer.writeheader()
