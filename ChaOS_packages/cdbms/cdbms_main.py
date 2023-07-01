4203d2bef4cba2ec8df7581f068fb66b8424adfe
kaf212 <kaf221122@gmail.com>
2023-07-01 10:12:04 +0200

    created primitive table formatting

--- ChaOS_packages/cdbms/cdbms_main.py
+++ ChaOS_packages/cdbms/cdbms_main.py
@@ -1,4 +1,5 @@
 from ChaOS_pm import install_python_package, import_functions
+import os
 
 
 def cdbms_main():
@@ -6,21 +7,23 @@ def cdbms_main():
     print('**** Welcome to the ChaOS Database Management System! ****')
     print('----------------------------------------------------------')
 
-    inp_username = input('Username > ')
-    inp_password = input('Password > ')
-    inp_database = input('Database > ')
+    inp_username = None  # input('Username > ')
+    inp_password = None  # input('Password > ')
+    inp_database = None  # input('Database > ')
 
     cnx = connect_to_db(inp_username, inp_password, inp_database)
 
     cursor = cnx.cursor(buffered=True)
+
     query = None
     while query != 'exit':
+        print('about to prompt query')
         query = input('Query > ')
         cursor.execute(query)
-        print(type(cursor))
 
-        for line in cursor:
-            print(line)
+        print_result(cursor)
+
+        input('Press enter to continue...')
 
     cursor.close()
     cnx.close()
@@ -38,9 +41,9 @@ def connect_to_db(username, password, database):
         errorcode = import_functions('mysql.connector', ['errorcode'])[0]
 
     try:
-        cnx = mysql.connector.connect(user=username, password=password,
-                                  host='127.0.0.1',
-                                  database=database)
+        cnx = mysql.connector.connect(user='root', password='1234',
+                                      host='127.0.0.1',
+                                      database='sakila')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password.")
@@ -53,6 +56,57 @@ def connect_to_db(username, password, database):
             return None
 
     return cnx
+
+
+def print_result(result_table):
+    def print_vertical_separator_line():
+        for i in range(max_record_length + ((len(result_table[0])) * 2)):  # print vertical separator line
+            print('-', end="")
+        print("")
+
+    print(type(result_table))
+    result_table = list(result_table)
+    print(type(result_table))
+    max_record_length = 0
+    for record in result_table:  # get the maximum char length of the record set
+        charlength = 0
+        for value in record:
+            charlength += len(str(value))
+        if charlength > max_record_length:
+            max_record_length = charlength
+            print(f'new max_record_length = {max_record_length}')
+
+
+    maximum_field_lengths = []
+    for i in range(len(result_table[0])):
+        maximum_field_lengths.append(0)
+
+    for record_1 in result_table:
+        for field_index, field in enumerate(record_1):
+            print(str(field_index))
+            if len(str(field)) > maximum_field_lengths[field_index]:
+                maximum_field_lengths[field_index] = len(str(field))
+
+        print(maximum_field_lengths)
+
+    print_vertical_separator_line()
+    for record in result_table:
+
+        print("|", end="")
+        for field_index, field in enumerate(record):
+            for i in range(int( (maximum_field_lengths[field_index] - len(str(field)))) + 1 ):
+                print(" ", end="")
+
+            print(field, end=" |")
+
+        print("")
+    print_vertical_separator_line()
+
+    """
+    for i in range(max_char_length + (len(res_tuple) * 2)):
+        print('-', end="")
+    """
+
 
 
 cdbms_main()
